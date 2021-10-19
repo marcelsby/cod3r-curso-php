efbce17a5eb0f3b03d3475fe3fb8248269abf05d
Marcel Barbosa <marcel.sby.br@gmail.com>
2021-10-19 11:29:27 -0300

    feat: template para os exercícios pronto

---
+++ src/exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/exercicio.css">
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelha">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            <?php 
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php")
+            ?>
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
