9a7e08d33ed618985d2a185de92f98e0efa07e54
Marcel Barbosa <marcel.sby.br@gmail.com>
2021-10-20 16:07:58 -0300

    feat: desafios do capítulo de tipos básicos do PHP

---
+++ src/tipos/desafio_string.php
@@ -0,0 +1,10 @@
+<div class="titulo">Desafio String</div>
+
+<?php
+
+// Enunciado
+// Avaliando os métodos da documentação da string,
+// qual é o método que retornará a posição do texto 'abc'
+// na string '!AbcaBcabc' com o valor 1?
+
+echo stripos('!AbcaBcabc', 'abc');
