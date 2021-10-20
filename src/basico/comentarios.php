1ba5852ec5784460ac04e209defc5505aff58893
Marcel Barbosa <marcel.sby.br@gmail.com>
2021-10-20 09:17:17 -0300

    feat: código das aulas do módulo 1
    Módulo: Integrando PHP com HTML & CSS

---
+++ src/basico/comentarios.php
@@ -0,0 +1,20 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php 
+echo "Estou no PHP";
+// echo "Comentário";
+# echo "Comentário";
+// echo # "Comentário";
+# echo // "Comentário";
+//?>
+
+<p>Depois do primeiro bloco</p>
+
+<?php 
+/*
+    Linha 1
+    Linha 2
+    Linha 3
+?> */ ?>
+
+<p>Depois do segundo bloco</p>
