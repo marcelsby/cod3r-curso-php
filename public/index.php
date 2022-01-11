2eb2d8240a53c1f5abf7f008fae54fcac9deb16f
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-11 19:22:51 -0300

    feat: conexão com o bd e consulta básica

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
-<h1>Ok</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+$sql = 'select * from users';
+$result = Database::getResultFromQuery($sql);
+
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
+    echo '<br>';
+}
--- src/config/database.php
+++ src/config/database.php
@@ -16,5 +16,16 @@ class Database
         if ($conn->connect_error) {
             die("Erro: " . $conn->connect_error);
         }
+
+        return $conn;
+    }
+
+    public static function getResultFromQuery($sql)
+    {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+
+        return $result;
     }
 }
