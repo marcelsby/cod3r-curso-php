6044bcfcf6cd5b49226e13603757bcd8b4c530ff
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-13 17:32:48 -0300

    feat: primeira tela de login feita
    Agora falta amarrar o front-end com o back-end, para a função de login passar a ser funcional.

--- public/index.php
+++ public/index.php
@@ -1,14 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-
-print_r(User::get(['name' => 'Quico'], 'name, email'));
-
-echo '<br>';
-
-foreach (User::get([], 'name') as $user) {
-    echo $user->name . '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
