7521b40e7a27978feb6b3a594086c7183718c9dc
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-13 17:00:04 -0300

    feat: classe Model incrementada e testada
    Para facilitar a realização de consultas no Banco de Dados, criamos alguns métodos na classe Model que abstraem as complexidades de lidar com consultas em cada model, adicionando um método para executar SELECTs com filtros (WHERE) e retornando apenas colunas necessárias.

--- public/index.php
+++ public/index.php
@@ -4,5 +4,11 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-$user->email = 'lucas_modificado@cod3r.com.br';
-print_r($user);
+
+print_r(User::get(['name' => 'Quico'], 'name, email'));
+
+echo '<br>';
+
+foreach (User::get([], 'name') as $user) {
+    echo $user->name . '<br>';
+}
--- src/models/Model.php
+++ src/models/Model.php
@@ -20,6 +20,73 @@ class Model
         }
     }
 
+    public static function get($filters = [], $columns = '*')
+    {
+        $objects = [];
+        $result = static::getResultSetFromSelect($filters, $columns);
+
+        if ($result) {
+            $class = get_called_class();
+            while ($row = $result->fetch_assoc()) {
+                array_push($objects, new $class($row));
+            }
+        }
+
+        return $objects;
+    }
+
+    public static function getResultSetFromSelect($filters = [], $columns = '*')
+    {
+        $sql = "SELECT ${columns} FROM "
+        . static::$tableName
+        . static::getFilters($filters)
+        . ';';
+
+        $result = Database::getResultFromQuery($sql);
+
+        if ($result->num_rows === 0) {
+            return null;
+        } else {
+            return $result;
+        }
+    }
+
+    private static function getFilters(array $filters)
+    {
+        $sql = '';
+
+        if (!empty($filters)) {
+            if (count($filters) == 1) {
+                foreach ($filters as $column => $value) {
+                    $sql .= " WHERE ${column} = " . static::getFormatedValue($value);
+                }
+            } else {
+                $index = 1;
+
+                foreach ($filters as $column => $value) {
+                    $sql .= $index == 1
+                        ? (" WHERE ${column} = " . static::getFormatedValue($value))
+                        : (" AND ${column} = " . static::getFormatedValue($value));
+
+                    $index++;
+                }
+            }
+        }
+
+        return $sql;
+    }
+
+    private static function getFormatedValue($value)
+    {
+        if (is_null($value)) {
+            return "null";
+        } elseif (gettype($value) === 'string') {
+            return "'${value}'";
+        } else {
+            return $value;
+        }
+    }
+
     public function __get($key)
     {
         return $this->values[$key];
