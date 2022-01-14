94188348d12adb6a84aa9a00ebe053dfc44e2544
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-14 18:21:28 -0300

    feat: loader e configurações estruturais projeto
    - Loader para facilitar o carregamento de Models, Utils e Views
    
    - O index agora chama o Controller de Login
    
    - Config ajustado para os novos arquivos e pastas do projeto

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,11 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Pastas do projeto
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models/'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views/'));
+define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers/'));
+define('UTILS_PATH', realpath(dirname(__FILE__) . '/../utils/'));
 
+// Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
+require_once(realpath(dirname(__FILE__) . '/loader.php'));
+require_once(MODEL_PATH . '/Model.php');
---
+++ src/config/loader.php
@@ -0,0 +1,26 @@
+<?php
+
+function loadModel($modelName)
+{
+    require_once(MODEL_PATH . "/{$modelName}.php");
+}
+
+function loadUtil($utilName)
+{
+    require_once(UTILS_PATH . "/{$utilName}.php");
+}
+
+function loadView($viewName, $params = [])
+{
+    loadUtil('variableValidation');
+
+    if (!empty($params)) {
+        foreach ($params as $key => $value) {
+            if (strlen($key) > 0 && isValidVariableIdentifier($key)) {
+                ${$key} = $value;
+            }
+        }
+    }
+
+    require_once(VIEW_PATH . "/{$viewName}.php");
+}
