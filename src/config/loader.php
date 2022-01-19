efbe43db1ef4311901ba5ba7783a2290eb52a873
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-19 16:33:19 -0300

    feat: base do template para as telas da aplicação

--- src/config/loader.php
+++ src/config/loader.php
@@ -37,5 +37,8 @@ function loadTemplateView($viewName, $params = [])
         }
     }
 
+    require_once(TEMPLATE_PATH . "/header.php");
+    require_once(TEMPLATE_PATH . "/left.php");
     require_once(VIEW_PATH . "/{$viewName}.php");
+    require_once(TEMPLATE_PATH . "/footer.php");
 }
