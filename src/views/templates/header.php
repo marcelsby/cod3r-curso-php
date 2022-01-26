261a0c322b001066caad9c6b7883ed4caacc6c9e
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-01-26 15:50:07 -0300

    feat: avatar do usuário no header

--- src/views/templates/header.php
+++ src/views/templates/header.php
@@ -25,7 +25,10 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span class="ml-3">Usuário Mock</span>
+                <img class="avatar"
+                    src="<?php echo 'http://www.gravatar.com/avatar.php?gravatar_id='
+                    . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="">
+                <span class="ml-3"><?php echo $_SESSION['user']->name ?></span>
                 <i class="icofont-simple-down mx-2"></i>
             </div>
             <div class="dropdown-content">
