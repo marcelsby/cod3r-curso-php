d6ba084eb2cb9eacdc917872c1d90d176c6d80df
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-02 15:57:11 -0300

    feat: funções utilitárias para manipular datas

---
+++ src/controllers/teste.php
@@ -0,0 +1,17 @@
+<?php
+
+// Controller temporário
+
+$i1 = DateInterval::createFromDateString('9 hours');
+$i2 = DateInterval::createFromDateString('2 hours');
+
+$r1 = sumIntervals($i1, $i2);
+$r2 = subtractIntervals($i1, $i2);
+
+print_r($r1);
+echo '<br>';
+print_r($r2);
+echo '<br>';
+print_r(getDateFromInterval($r1));
+echo '<br>';
+print_r(getDateFromInterval($r2));
