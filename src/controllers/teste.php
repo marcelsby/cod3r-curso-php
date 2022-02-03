9b475c7dd98a309ae601231dcc796fbffa4d9725
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-03 18:41:50 -0300

    feat: cálculo da hora de saída

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -6,10 +6,16 @@ loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
 
+echo 'Tempo trabalhado: ';
 $workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
 print_r($workedIntervalString);
 echo '<br>';
 
+echo 'Tempo de almoço: ';
 $lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
 print_r($lunchIntervalString);
+echo '<br>';
+
+echo 'Horário de saída: ';
+print_r($wh->getExitTime()->format('H:i:s'));
 echo '<br>';
--- src/models/WorkingHours.php
+++ src/models/WorkingHours.php
@@ -2,6 +2,8 @@
 
 class WorkingHours extends Model
 {
+    private static $workday;
+    private static $defaultLunchTime;
     protected static $tableName = 'working_hours';
     protected static $columns = [
         'id',
@@ -16,6 +18,9 @@ class WorkingHours extends Model
 
     public static function loadFromUserAndDate($userId, $workDate)
     {
+        self::$workday = DateInterval::createFromDateString('8 hour');
+        self::$defaultLunchTime = DateInterval::createFromDateString('1 hour 30 minutes');
+
         $registry = self::getOne(['user_id' => $userId, 'work_date' => $workDate]);
 
         if (!isset($registry)) {
@@ -85,17 +90,38 @@ class WorkingHours extends Model
     public function getLunchInterval()
     {
         [, $t2, $t3,] = $this->getTimes();
-        $breakInterval = new DateInterval('PT0S');
+
+        if (!isset($t2) && !isset($t3)) {
+            return self::$defaultLunchTime;
+        }
+
+        $lunchInterval = new DateInterval('PT0S');
 
         if ($t2) {
-            $breakInterval = $t2->diff(new DateTime());
+            $lunchInterval = $t2->diff(new DateTime());
         }
 
         if ($t3) {
-            $breakInterval = $t3->diff($t2);
+            $lunchInterval = $t2->diff($t3);
         }
 
-        return $breakInterval;
+
+        return $lunchInterval;
+    }
+
+    public function getExitTime()
+    {
+        [$t1, , , $t4] = $this->getTimes();
+
+        if (!$t1) {
+            $exitTime = (new DateTimeImmutable())->add(self::$workday)->add(self::$defaultLunchTime);
+        } elseif ($t4) {
+            $exitTime = $t4;
+        } else {
+            $exitTime = $t1->add(sumIntervals(self::$workday, $this->getLunchInterval()));
+        }
+
+        return $exitTime;
     }
 
     private function getTimes()
